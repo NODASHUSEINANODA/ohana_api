e5be125a69f1a11542bb032acbbc6689adfe7164
miyagi <miyagi@synchro.co.jp>
2023-04-09 00:16:52 +0900

    issue/#20 fix : migrationファイルを作り直し、正しい順番で実行されるようにした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_02_142423) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_08_150541) do
   create_table "companies", charset: "utf8mb4", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
