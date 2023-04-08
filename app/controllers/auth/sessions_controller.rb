46d51f9411ebf765362ee9f7f34618a606e42c3d
miyagi <miyagi@synchro.co.jp>
2023-04-09 00:17:58 +0900

    issue/#20 feat : current_userがいない場合は401でエラーを返す

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -1,9 +1,6 @@
 class Auth::SessionsController < ApplicationController
     def index
-        if current_user
-            render json: {is_login: true, data: current_user }
-        else
-            render json: {is_login: false, message: "ユーザーが存在しません"}
-        end
+        render json: {is_login: true, data: current_user } if current_user
+        render json: {is_login: false, message: "ユーザーが存在しません"}, status: 401
     end
 end
