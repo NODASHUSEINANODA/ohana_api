ebd0016642dd6f13dd4614b1a9373b914b927bf8
tamashiro-syuta <tamashiro_syuta@tamakinoMacBook-Pro.local>
2023-04-15 00:23:14 +0900

    feat : 花屋へメールを送信する際のバッチ処理の雛形を作成

---
+++ lib/tasks/send_mail/to_flower_shop.rb
@@ -0,0 +1,9 @@
+class Tasks::SendMail::ToFlowerShop < Tasks::AbstBatch
+  def self.exec
+    execute('花屋さんへメールを送信します') do
+      # TODO: ここにメールを送信する処理を書く
+      puts 'hogehoge'
+      raise RuntimeError
+    end
+  end
+end
