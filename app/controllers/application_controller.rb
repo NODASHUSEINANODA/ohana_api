f8dfd64b09755dce6537057ac2800e6a78bf9c39
tamashiro-syuta <tamashiro_syuta@tamakinoMacBook-Pro.local>
2023-04-27 22:02:07 +0900

    fix : API用ではない通常モードと同じ継承元から継承

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
         # Cookie や CORS の設定ができるようになる
         include DeviseTokenAuth::Concerns::SetUserByToken
 end
