ab5ef90dd62aa6f1ed7fbd29c631258d34a99249
tamashiro-syuta <tamashiro_syuta@tamakinoMacBook-Pro.local>
2023-05-02 20:48:50 +0900

    feat : jquery関連の設定を追加

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,11 @@
 import { Application } from "@hotwired/stimulus"
+import jquery from "jquery"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
+window.$ = jquery
 
 export { application }
