342224bc8ffd0ff9314a8ee781fb1ff0d98e8c8d
Yuki-Miyagi <codegym.yuki@gmail.com>
2023-05-06 16:58:23 +0900

    issue/#34 fix : devise_token_authで使用していたコントローラとルーティングの設定を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
-        # Cookie や CORS の設定ができるようになる
-        include DeviseTokenAuth::Concerns::SetUserByToken
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :companies
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  # ログイン機能のルーティング
-  mount_devise_token_auth_for 'Company', at: 'auth', controllers: {
-    registrations: 'auth/registrations'
-  }
-  # ログインユーザー取得のルーティング
-  namespace :auth do
-    resources :sessions, only: %i[index]
-  end
 
   namespace 'api' do
     get '/healthcheck', to: 'healthcheck#index'
