55b1cce3213985eaff2948334e72993a07d244ed
Yuki-Miyagi <codegym.yuki@gmail.com>
2023-05-09 18:26:45 +0900

    issue/#34 feat : sign_up時にname, addressに個別でバリデーションをかけた

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,11 @@
 class Company < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  validates :name, presence: true, length: { maximum: 20, allow_blank: true }
+  validates :address, presence: true, length: { maximum: 20, allow_blank: true }
+  
+  # この:validatableはpasswordとemailしか検証してくれない
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
