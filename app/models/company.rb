078395eb8c1b2e02ce3247ee132ad8238c145923
tamashiro-syuta <syuta5825@gmail.com>
2023-06-08 02:04:05 +0900

    feat : 今月誕生日の情報を取得し、メールを送信する処理

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,7 @@
 class Company < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :employees, dependent: :destroy
   has_one :flower_shop
 
   validates :name, presence: true, length: { maximum: 20, allow_blank: true }
@@ -10,4 +11,17 @@ class Company < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  def current_month_order_to_flower_shop
+    members = self.birthday_in_current_month_members
+
+    CompanyMailer.with(members: members).order_to_flower_shop.deliver_now if members.present?
+  end
+
+  private
+
+  def birthday_in_current_month_members
+    current_month = Time.zone.now.strftime("%m")
+    birthday_in_current_month_condition = Employee.arel_table[:birthday].extract('month').eq(current_month)
+    employees.where(birthday_in_current_month_condition)
+  end
 end
