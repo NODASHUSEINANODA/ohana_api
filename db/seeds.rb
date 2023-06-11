c2d15d7d45d0a80b6ee7d90327eda25efa4dcf0c
tamashiro-syuta <syuta5825@gmail.com>
2023-06-11 14:32:10 +0900

    テストデータを変更した外部キーに合わせて、カラムと生成する順番を変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,23 @@
 3.times do |n|
+  FlowerShop.create!(
+    name: Gimei.name.last.kanji.concat('花屋'),
+    email: "flower#{n + 1}@example.com",
+    created_at: Time.current,
+    updated_at: Time.current
+  )
+end
+
+# flower_shop_idはflower_shopのデータが作成されていることが前提、順番変えるとうまくいかない
+3.times do |n|
+  flower_shop_id_first = FlowerShop.first.id
+  flower_shop_id_last = FlowerShop.last.id
   Company.create!(
     email: "test#{n + 1}@example.com",
     password: 'password',
     name: "test#{n}株式会社",
     address: Gimei.unique.address,
-    confirmed_at: Time.current
+    confirmed_at: Time.current,
+    flower_shop_id: Random.rand(flower_shop_id_first..flower_shop_id_last)
   )
 end
 
@@ -14,24 +27,12 @@ end
   s = Random.rand(s1..s2)
   Employee.create!(
     name: Gimei.unique.name.kanji,
-    sex: Random.rand(1..2) == 2 ? '男' : '女',
+    sex: Random.rand(1..2) == 2 ? '男性' : '女性',
     birthday: s,
     address: Gimei.unique.address,
     joined_at: s,
     phone_number: '09011112222',
     message: Random.rand(1..2) == 2 ? '情熱的' : '落ち着いている',
     company_id: Random.rand(1..3)
-  )
-end
-
-# company_idはcompanyのデータが作成されていることが前提、順番変えるとうまくいかない
-
-3.times do |n|
-  FlowerShop.create!(
-    name: Gimei.name.last.kanji.concat('花屋'),
-    email: "flower#{n + 1}@example.com",
-    created_at: Time.current,
-    updated_at: Time.current,
-    company_id: Company.find_by(id: n + 1).id
   )
 end
