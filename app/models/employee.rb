0d4209f5d6cc5e2f250a053e547996e87f00a835
tamashiro-syuta <syuta5825@gmail.com>
2023-06-20 00:58:50 +0900

    fix : manager追加の際にcompany_idを追加

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -18,7 +18,7 @@ class Employee < ApplicationRecord
   def save_with_manager(email, is_president)
     transaction do
       save!
-      manager = Manager.new(employee_id: id, email: email, is_president: is_president)
+      manager = Manager.new(employee_id: id, email: email, is_president: is_president, company_id: company_id)
       manager.save!
     end
   end
