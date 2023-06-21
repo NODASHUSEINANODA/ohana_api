fe39f75196281e1b9c093b821a884747bf45c40e
tamashiro-syuta <syuta5825@gmail.com>
2023-06-21 23:13:35 +0900

    chore : 住所と電話番号のnull制約を外した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_17_035138) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_21_140302) do
   create_table "companies", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -35,8 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_035138) do
     t.string "name", null: false
     t.string "sex", null: false
     t.datetime "birthday", null: false
-    t.string "address", null: false
-    t.string "phone_number", null: false
+    t.string "address"
+    t.string "phone_number"
     t.text "message"
     t.bigint "company_id", null: false
     t.datetime "created_at", null: false
@@ -69,8 +69,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_035138) do
     t.boolean "is_president", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "company_id", null: false
-    t.index ["company_id"], name: "index_managers_on_company_id"
     t.index ["employee_id"], name: "index_managers_on_employee_id"
   end
 
