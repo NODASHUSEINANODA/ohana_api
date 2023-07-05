cc15f727cb06d3e7c21c13619b605f10fcacfe2e
Yuki-Miyagi <codegym.yuki@gmail.com>
2023-07-05 22:06:57 +0900

    issue/#91 chore : healthcheck, api削除

--- app/controllers/api/healthcheck_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# SPA構成じゃなくなったので、不要だが、viewが見れる状態になったことを示すために残しておく
-# TODO プルリクで確認してもらった後に削除する
-class Api::HealthcheckController < ApplicationController
-  def index
-    @hoge = 'hoge'
-  end
-end
