98dce4b1bab4b1684f7027ada0d250e84a7c28b3
tamashiro-syuta <syuta5825@gmail.com>
2023-07-09 13:46:58 +0900

    feat : OrderMailerに注文結果に応じたメールを作成

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,26 @@
 class OrderMailer < ApplicationMailer
+  before_action :set_president_name_and_email
+
+  default from: Settings[:SYSTEM_MAIL_ADDRESS]
+
+  def shipping_confirmation_to_president
+    @next_orders_info = params[:next_orders_info]
+
+    mail(
+      to: @president_email,
+      subject: '【ONE-STEP-GIFT】翌月の注文に関するリマインド'
+    )
+  end
+
+  def no_shipping_confirmation_to_president
+    mail(
+      to: @president_email,
+      subject: '【ONE-STEP-GIFT】翌月の注文に関するリマインド'
+    )
+  end
+
+  def set_president_name_and_email
+    @president_name = params[:president_name]
+    @president_email = params[:president_email]
+  end
 end
