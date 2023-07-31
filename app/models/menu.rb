208c23aa7a1f04386e0c644809adfb1d3f0cea06
Yuki-Miyagi <codegym.yuki@gmail.com>
2023-07-31 20:50:22 +0900

    issue/#116 feat : カラムの追加とenumerizeの設定

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -4,9 +4,16 @@ class Menu < ApplicationRecord
   has_many :order_details
   belongs_to :flower_shop
 
-  validates :name, :price, :flower_shop_id, presence: true
+  validates :name, :price, :flower_shop_id, :season, presence: true
 
   scope :cheapest, -> { order(price: :asc).first }
+
+  enumerize :season, in: {
+    spring: 1,
+    summer: 2,
+    automn: 3,
+    winter: 4
+  }, scope: true
 
   def name_with_price
     "#{name}(#{price}円)"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_29_080330) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_31_113525) do
   create_table "companies", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_29_080330) do
     t.bigint "flower_shop_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "season", limit: 1
     t.index ["flower_shop_id"], name: "index_menus_on_flower_shop_id"
   end
 
