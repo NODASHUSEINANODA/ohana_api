4460e8a2093fdf2f15791791db58bbe4463fd373
tamashiro-syuta <syuta5825@gmail.com>
2023-08-14 22:25:13 +0900

    ドメインをもうちょい詳しくしてみた

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,11 +100,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     :address              => "mail12.onamae.ne.jp",
-    :domain               => "ne.jp",
+    :domain               => "onamae.ne.jp",
     :port                 => 465,
     :user_name            => "no-reply@one-step-gift.com",
     :password             => ENV.fetch("ONAMAE_MAIL_SMTP_PASSWORD"),
-    :authentication       => :plain,
+    :authentication       => :login,
     :enable_starttls_auto => true,
   }
 end
